e9c8cafab8a116a0db6fb5bf46af40cbff10dbba
amarcozzi <anthonymarcozzi@gmail.com>
2026-03-20 05:09:10 -0600

    enhance grid validation by adding chunk shape check and update response model

--- services/api/api/resources/grids/data/zarr.py
+++ services/api/api/resources/grids/data/zarr.py
@@ -11,12 +11,12 @@ from zarr import AsyncArray, AsyncGroup
 from lib.config import GRIDS_BUCKET
 
 
-@lru(force_asyncio=True)
+@lru(maxsize=128, force_asyncio=True)
 async def get_grid_group(grid_id: str) -> AsyncGroup:
     return await zarr_async.open_group(store=f"gs://{GRIDS_BUCKET}/{grid_id}", mode="r")
 
 
-@lru(force_asyncio=True)
+@lru(maxsize=128, force_asyncio=True)
 async def get_grid_array(grid_id: str, band_key: str) -> AsyncArray:
     group = await get_grid_group(grid_id)
     return await group.getitem(band_key)
